7f1979de6893a174ec72a4116a3886a28d74a694
geezlothbrok <giddyohene5@gmail.com>
2025-03-31 23:05:52 +0000

    complete image upload functionality

--- client/src/pages/auth/Profile.jsx
+++ client/src/pages/auth/Profile.jsx
@@ -1,15 +1,89 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./SignUp.css";
-import avatar from "../../assets/images/display_photo.jpg";
 import { useSelector } from "react-redux";
+import { app } from "../../firebase/firebase.config";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 function Profile() {
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        console.error("Upload error:", error);
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
-    <div className="signup-container" style={{paddingBlock: "1rem"}}>
+    <div className="signup-container" style={{ paddingBlock: "1rem" }}>
       <h1 className="signup-title">profile</h1>
-      <form className="form-container" style={{marginTop: 0}}>
-        <img src={currentUser.avatar }  alt="profile" style={{borderRadius: "50%", marginBlock: "1rem", height: "200px", width: "200px", objectFit: "cover"}}/>
+      <form className="form-container" style={{ marginTop: 0 }}>
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <img
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          style={{
+            borderRadius: "50%",
+            marginBlock: "1rem",
+            height: "200px",
+            width: "200px",
+            objectFit: "cover",
+            cursor: "pointer",
+          }}
+          onClick={() => fileRef.current.click()}
+        />
+        <p>
+          {fileUploadError ? (
+            <span style={{color: "red", fontSize: "12px"}}>Error in Uploading image</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span style={{color: "#58d68d", fontSize: "12px"}}>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span style={{color: "#186a3b", fontSize: "12px"}}>Image Uploaded Successfully!</span>
+          ) : (
+            ""
+          )}
+        </p>
+
         <input
           type="text"
           placeholder="username"
@@ -44,7 +118,11 @@ function Profile() {
         <button
           type="submit"
           className="submit"
-          style={{ marginTop: 20, backgroundColor: "#4caf50", letterSpacing: 1 }}
+          style={{
+            marginTop: 20,
+            backgroundColor: "#4caf50",
+            letterSpacing: 1,
+          }}
           // disabled={loading}
         >
           update
@@ -52,28 +130,47 @@ function Profile() {
         <button
           type="submit"
           className="submit"
-          style={{ marginTop: 20, backgroundColor: "#2c3e50" , color: "wheat", letterSpacing: 1}}
+          style={{
+            marginTop: 20,
+            backgroundColor: "#2c3e50",
+            color: "wheat",
+            letterSpacing: 1,
+          }}
           // disabled={loading}
         >
           create listing
         </button>
-        
+
         <div className="already-account">
           <span
             className="already-link"
-            style={{ textTransform: "capitalize", color: "red", cursor: "pointer" }}
+            style={{
+              textTransform: "capitalize",
+              color: "red",
+              cursor: "pointer",
+            }}
           >
             delete account
           </span>
 
           <span
             className="already-link"
-            style={{ textTransform: "capitalize", cursor: "pointer", marginLeft: "3rem", color: "red", }}
+            style={{
+              textTransform: "capitalize",
+              cursor: "pointer",
+              marginLeft: "3rem",
+              color: "red",
+            }}
           >
             sign out
           </span>
         </div>
-        <p className="show-listing" style={{color: "#186a3b", cursor: "pointer", fontWeight: "bold",}}>Show Listings</p>
+        <p
+          className="show-listing"
+          style={{ color: "#186a3b", cursor: "pointer", fontWeight: "bold" }}
+        >
+          Show Listings
+        </p>
         {/* {error && <p className="error">{error}</p>} */}
       </form>
     </div>
