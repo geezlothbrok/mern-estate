e9c0dbcc37a01b8afee6f644549cdbe78b07295b
geezlothbrok <giddyohene5@gmail.com>
2025-04-09 22:51:48 +0000

    complete search listing api routes

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -69,3 +69,49 @@ export const getListing = async (req, res, next) => {
         
     }
 }
+
+
+// Get all listings with optional filters and pagination
+// @route GET /api/listing/get
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let furnished = req.query.furnished;
+
+        if(furnished === undefined || furnished === false) {
+            furnished = {$in: [false, true]};
+        };
+
+        let parking = req.query.furnished;
+
+        if(parking === undefined || parking === false) {
+            parking = {$in: [false, true]};
+        };
+
+        let type = req.query.furnished;
+
+        if(type === undefined || type === "all") {
+            type = {$in: ["sale", "rent"]};
+        };
+
+        const searcTerm = req.query.searchTerm || "";
+
+        const sort = req.query.sort || "createdAt";
+
+        const order = req.query.order || "desc";
+
+        const listings = await Listing.find({
+            name: {$regex: searcTerm, $options: "i" },
+            furnished,
+            parking,
+            type,
+        }).sort({ [sort]: order }).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+        
+    }
+}
