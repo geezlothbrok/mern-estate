9d92a6c616e0f70128a9ebe56a54152e24cf34b6
geezlothbrok <giddyohene5@gmail.com>
2025-04-15 22:38:32 +0000

    adding more search options to the search page

--- client/src/pages/search/Search.jsx
+++ client/src/pages/search/Search.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React from "react";
 import "./Search.css";
 
 function Search() {
   return (
-    <main className='search-main'>
-        <section className="left-side">
-            <form>
-                <div className="search-term">
-                    <label>Search Term:</label>
-                    <input type="text" id='search' placeholder='search...' className='look-for'/>
-                </div>
-               
-                <div className="sea">
-                    <label>Type:</label>
-                    <input type="checkbox" id="all"/>
-                    <span>Rent & Sale</span>
-                </div>
-            </form>
-        </section>
-        <section className="right-side">
-            <h1>Listing results:</h1>
-        </section>
+    <main className="search-main">
+      <section className="left-side">
+        <form>
+          <div className="search-term">
+            <label>Search Term:</label>
+            <input
+              type="text"
+              id="search"
+              placeholder="search..."
+              className="look-for"
+            />
+          </div>
+          <div className="all-search">
+            <div className="sea">
+              <label>Type:</label>
+              <input type="checkbox" id="all" />
+              <span>Rent & Sale</span>
+            </div>
+            <div className="hello">
+              <input type="checkbox" id="rent" />
+              <span>Rent</span>
+            </div>
+            <div className="">
+              <input type="checkbox" id="sale" />
+              <span>Sale</span>
+            </div>
+          </div>
+          <div className="all-search">
+            <div className="sea">
+              <label>Aminities:</label>
+              <input type="checkbox" id="all" />
+              <span>Parking</span>
+            </div>
+            <div className="hello">
+              <input type="checkbox" id="rent" />
+              <span>Furnished</span>
+            </div>
+            
+          </div>
+        </form>
+      </section>
+      <section className="right-side">
+        <h1>Listing results:</h1>
+      </section>
     </main>
-  )
+  );
 }
 
-export default Search
+export default Search;
