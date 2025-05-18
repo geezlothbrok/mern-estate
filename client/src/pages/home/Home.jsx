b6b9a287aa630c89c52f59a29c303a8f753483fc
geezlothbrok <giddyohene5@gmail.com>
2025-05-18 21:49:11 +0000

    added more styles to the home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import "./Home.css";
+import { BiBuildingHouse } from "react-icons/bi";
+import { LiaWarehouseSolid } from "react-icons/lia";
+import { MdOutlineVilla } from "react-icons/md";
+import { PiBuildingApartmentLight } from "react-icons/pi";
+import { RiHome9Line } from "react-icons/ri";
+
+
 
 function Home() {
   return (
@@ -8,6 +15,30 @@ function Home() {
         <div className="background-image">
           <div className="hero-background-title">
             <h1 className="background-title">Lets find your dream house.</h1>
+          </div>
+        </div>
+        <p style={{color: "#d35400", textAlign: "center", marginTop: "10px"}}>Property by Requirement</p>
+        <h2 style={{color: "#ffff", textAlign: "center", marginTop: "10px", marginBottom: "30px"}}>Explore Appartment <span style={{color: "#1a8f4d"}}>Types</span></h2>
+        <div className="svg-container">
+          <div className="svg-card">
+          <BiBuildingHouse className='svgs' />
+          <h3>Commercial</h3>
+          </div>
+          <div className="svg-card">
+          <LiaWarehouseSolid className='svgs'/>
+          <h3>Warehouse</h3>
+          </div>
+          <div className="svg-card">
+          <MdOutlineVilla className='svgs'/>
+          <h3>Villa</h3>
+          </div>
+          <div className="svg-card">
+          <PiBuildingApartmentLight className='svgs'/>
+          <h3>Apartment</h3>
+          </div>
+          <div className="svg-card">
+          <RiHome9Line className='svgs'/>
+          <h3>Homestay</h3>
           </div>
         </div>
       </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from  'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 })
