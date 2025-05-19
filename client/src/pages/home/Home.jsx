a8c084188c0aa5ee11bf8b06c8ee4d052d8ef846
geezlothbrok <giddyohene5@gmail.com>
2025-05-19 07:33:39 +0000

    added intro section to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import { LiaWarehouseSolid } from "react-icons/lia";
 import { MdOutlineVilla } from "react-icons/md";
 import { PiBuildingApartmentLight } from "react-icons/pi";
 import { RiHome9Line } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 
 
@@ -17,8 +18,8 @@ function Home() {
             <h1 className="background-title">Lets find your dream house.</h1>
           </div>
         </div>
-        <p style={{color: "#d35400", textAlign: "center", marginTop: "10px"}}>Property by Requirement</p>
-        <h2 style={{color: "#ffff", textAlign: "center", marginTop: "10px", marginBottom: "30px"}}>Explore Appartment <span style={{color: "#1a8f4d"}}>Types</span></h2>
+        <p style={{color: "#d35400", textAlign: "center", marginTop: "10px"}}>Property By Requirement</p>
+        <h2 style={{color: "#ffff", textAlign: "center", marginTop: "10px", marginBottom: "30px"}}>Explore Apartment <span style={{color: "#1a8f4d"}}>Types</span></h2>
         <div className="svg-container">
           <div className="svg-card">
           <BiBuildingHouse className='svgs' />
@@ -41,6 +42,19 @@ function Home() {
           <h3>Homestay</h3>
           </div>
         </div>
+      </div>
+      <div className="intro-sect">
+        <h1 className="intro-header">
+          Find your <span style={{fontWeight: 700, color: "#2c3e50", lineHeight: 2}}>perfect</span> dream home with us! <br />
+          We make it easy to find your dream home. <br />
+        </h1>
+        <p className="intro-text">
+          We are a team of real estate experts dedicated to helping you find your dream home. <br />
+          Our mission is to make the process of buying or selling a home as easy and stress-free as possible. <br />
+          We understand that finding the right home can be overwhelming, which is why we are here to help you every step of the way. <br />
+          Whether you are a first-time homebuyer or an experienced investor, we have the knowledge and expertise to help you achieve your real estate goals. <br />
+          </p>
+          <Link to = "/search" style={{color: "#1a8f4d", fontWeight: 700}}>Let's get started...</Link>
       </div>
     </main>
   )
